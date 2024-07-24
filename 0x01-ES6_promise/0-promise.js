3408415dfebb1942372759e6f8b21652953f9002
bmwangangi <trabbart8@gmail.com>
2024-07-24 23:25:32 +0300

    corrected

--- 0x01-ES6_promise/0-promise.js
+++ 0x01-ES6_promise/0-promise.js
@@ -1,6 +1,5 @@
 export default function getResponseFromAPI() {
-  return new Promise((resolve, reject) => {
-      // This can be a mockup of an API call, for now, we can just resolve it immediately
-      resolve("API response");
+  return new Promise((resolve) => {
+      resolve('API response');
   });
 }

30e62a45798cdcec1018de8dd7fc21fad9142390
bmwangangi <trabbart8@gmail.com>
2024-07-25 21:03:22 +0300

    0-classroom done

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,5 +1,5 @@
-export default class {
+export default class ClassRoom {
   constructor(maxStudentsSize) {
-    this._maxStdentsSize = maxStudentsSize;
+    this._maxStudentsSize = maxStudentsSize;
   }
 }
